f6c767c7d4a7c47429ecce568a63970ad077371e
pfviktor <viktocode@gmail.com>
2023-02-07 11:43:20 +0100

    sortBy done

--- sortBy.js
+++ sortBy.js
@@ -2,6 +2,10 @@ const getBeers = require('./data')
 
 function main(property) {
     console.log('property:', property)
+    const beers = getBeers();
+    const sorted = [...beers];
+    sorted.sort((left, right) => left.abv - right.abv)
+    beers.forEach(beer => console.log(`${beer.name} ${beer.brewery}`))
 
 }
 
